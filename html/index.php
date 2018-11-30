b852cda0835322da7dbccf865e51c22dfacd1e5e
joelparrat <joel.parrat@gmail.com>
2018-11-30 10:19:30 +0100

    plus image

---
+++ html/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>Projet DHT11</title>        
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+
+    <body>
+		<div>
+			coucou
+		</div>
+		
+		<?php include("../php/fonction.php"); ?>
+		<script src="../js/script.js"></script>
+    </body>
+
+</html>
