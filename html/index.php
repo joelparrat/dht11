f01e9d58c2b79ad934dbf3462dacd70d7258b5ea
joelparrat <joel.parrat@gmail.com>
2018-11-30 12:28:32 +0100

    afficher temperature

--- html/index.php
+++ html/index.php
@@ -11,7 +11,24 @@
 
     <body>
 		<div>
-			coucou
+			<h1>Thermometre</h1> 
+		</div>
+		<p>
+		<?php
+			$txt = file_get_contents("../txt/objet.json");
+			//$fch = fopen("../txt/objet.json", "r");
+			//$txt = fgets($fch);
+			$objJSON = json_decode($txt);
+			echo  "Il fait ".$objJSON->temperature."°C avec ".$objJSON->humidite."% d'humidite.";
+			//fclose($fch);
+			//echo date();
+			$baragraph_height = 161 + $objJSON->temperature * 4;
+			$baragraph_top = 315 - $objJSON->temperature * 4;
+		?>
+		</p>
+		<div id="thermometer">
+			<div id="bargraph" style="height:<?php echo $baragraph_height; ?>px; top:<?php echo $baragraph_top; ?>px;">
+			</div>
 		</div>
 		
 		<?php include("../php/fonction.php"); ?>
